569d1971c7538459a167b701459cc988355c6879
Alexandr Kirikovich <mail@akirikovich.ru>
2015-08-20 18:02:53 +0300

    Jade

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = function() {
+	return {
+		"db" : {
+			"host": "localhost",
+			"port": "3308",
+			"database": "ml",
+			"user": "goodcode",
+			"password": "6N9w9W5q"
+		}
+	}
+}();
--- server.js
+++ server.js
@@ -1,14 +1,81 @@
 var express = require('express'),
 	app = express(),
+	bodyParser = require('body-parser'),
 	fs = require('fs'),
 	parse = require('csv-parse'),
 	iconv = require('iconv-lite'),
-	jsObfuscator = require('js-obfuscator');
+	jsObfuscator = require('js-obfuscator'),
+	session = require('express-session'),
+	md5 = require('md5');
 
-app.route('/')
+var User = require('./lib/user.js');
+
+
+// Common application settings
+app.set("public", __dirname + "/public");
+app.set("views", app.get("public") + "/views");
+
+// Set jade as default rendoer for html pages
+app.set("view engine", "jade");
+
+// Body requests parser
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+// Sessions settings
+app.use(session({
+	secret: md5("ml"),
+	resave: false,
+	saveUninitialized: true,
+	name: "ml",
+	cookie: {
+		secure: true
+	}
+}));
+
+//app.use(express.static('public'));
+
+
+
+
+
+// Home page
+app.route("/")
 	.get(function(req, res) {
-		res.sendFile(__dirname + '/index.html');
+		res.render("index");
 	});
+
+app.route("/personal/")
+	.get(function(req, res) {
+		console.log(req.session.user);
+		res.sendFile(__dirname + '/public/personal.html');
+	});
+
+/*** Users authorization ***/
+app.route('/login/')
+	.post(function(req, res) {
+		var login = req.body["login-email"],
+			password = req.body["login-password"];
+
+		var userInfo = User.login(login, password, function(info) {
+			if(info) {
+				req.session.regenerate(function() {
+					req.session.user = info;
+					res.redirect("/personal/");
+				});
+			} else {
+				res.redirect("/login/");
+			}
+		});
+
+
+
+	})
+	.get(function(req, res) {
+		res.sendFile(__dirname + '/public/index.html');
+	});
+/*** //Users authorization ***/
 
 app.route('/get/')
 	.get(function(req, res) {
